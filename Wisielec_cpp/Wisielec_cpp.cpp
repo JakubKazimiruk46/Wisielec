d15ed82fad18dc7b161dd50ad2ab6bbfaea2b467
Jakub Kaz <jakubkazimiruk@gmail.com>
2023-12-27 12:40:25 +0100

    First commit

---
+++ Wisielec_cpp/Wisielec_cpp.cpp
@@ -0,0 +1,10 @@
+﻿// Wisielec_cpp.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  
+}
